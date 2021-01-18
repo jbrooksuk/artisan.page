044468d0017aecdacd8892c39029331b96e8610a
James Brooks <james@alt-three.com>
2021-01-18 21:30:15 +0000

    Fix shadow overflow issue

--- components/Command.vue
+++ components/Command.vue
@@ -1,7 +1,7 @@
 <template>
-    <div :id="slug" class="shadow-lg">
-        <div class="rounded-t-xl overflow-hidden bg-white p-10">
-            <h1 class="text-xl font-semibold text-indigo-900">
+    <div :id="slug" class="shadow-lg rounded-lg overflow-hidden">
+        <div class="rounded-t-lg overflow-hidden bg-white p-10">
+            <h1 class="text-lg font-semibold text-indigo-900">
                 <a :href="`#${slug}`">{{ command.name }}</a>
             </h1>
 
@@ -44,8 +44,8 @@
             </div>
         </div>
 
-        <div class="overflow-hidden rounded-b-xl bg-gradient-to-r from-indigo-900 to-indigo-700">
-            <pre class="scrollbar-none overflow-x-auto p-6 text-sm leading-snug text-white bg-black bg-opacity-40">php artisan {{ command.synopsis }}</pre>
+        <div class="overflow-hidden rounded-b-lg bg-gradient-to-r from-indigo-900 to-indigo-700">
+            <pre class="scrollbar-none overflow-hidden overflow-x-auto p-6 text-sm leading-snug text-white bg-black bg-opacity-40">php artisan {{ command.synopsis }}</pre>
         </div>
     </div>
 </div>
