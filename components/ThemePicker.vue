8d7b9a2c56ebebc41b1fd6054212118635e91e21
Ash Allen <mail@ashallendesign.co.uk>
2021-12-24 00:46:54 +0000

    Updated theme picker to a dropdown.

--- components/ThemePicker.vue
+++ components/ThemePicker.vue
@@ -1,43 +1,10 @@
 <template>
-  <div
-    @click="
-      $colorMode.preference =
-        $colorMode.preference === 'dark' ? 'light' : 'dark'
-    "
-    class="my-3 ml-5 h-12 w-12 cursor-pointer border-2 rounded-md flex justify-center items-center text-indigo-900 bg-gray-100 hover:bg-gray-200 hover:border-indigo-400 hover:text-indigo-400 dark:bg-gray-800 dark:border-indigo-400 dark:text-yellow-200 dark:hover:bg-gray-900 dark:hover:text-indigo-400"
+  <select
+    v-model="$colorMode.preference"
+    class="ml-3 block pl-3 pr-10 py-2 text-base border border-gray-300 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm rounded-md dark:bg-gray-800 dark:text-gray-300"
   >
-    <!-- sun -->
-    <svg
-      v-if="$colorMode.preference === 'dark'"
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-7 w-7"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-      />
-    </svg>
-
-    <!-- moon -->
-    <svg
-      v-else
-      xmlns="http://www.w3.org/2000/svg"
-      class="h-6 w-6"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-      />
-    </svg>
-  </div>
+    <option value="system">🖥 System</option>
+    <option value="light">☀️ Light</option>
+    <option value="dark">🌙 Dark</option>
+  </select>
 </template>
