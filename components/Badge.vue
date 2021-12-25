b8e84b452e804c2f29de0b380185a3e53e97fe81
James Brooks <jbrooksuk@users.noreply.github.com>
2021-12-25 22:55:41 +0000

    Apply fixes from StyleCI

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,14 +1,6 @@
 <template>
   <span
-    class="
-      inline-flex
-      items-center
-      px-2.5
-      py-0.5
-      rounded-md
-      text-sm
-      font-medium
-    "
+    class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium"
     :class="{
       'bg-red-100 text-red-800': required,
       'bg-green-100 text-green-800': !required,
