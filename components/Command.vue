e8b7410897bc2c695592bf6769b5a963dd8aadac
Mehmet Celal Kara <info@mcelal.com>
2022-11-04 12:05:53 +0300

    Fix lint errors

--- components/Command.vue
+++ components/Command.vue
@@ -1,8 +1,6 @@
 <template>
-  <div
-    :id="slug"
-    class="shadow-lg rounded-lg overflow-hidden bg-white dark:bg-gray-800 dark:border-2 dark:border-gray-200"
-  >
+  <div :id="slug"
+    class="shadow-lg rounded-lg overflow-hidden bg-white dark:bg-gray-800 dark:border-2 dark:border-gray-200">
     <div class="rounded-t-lg bg-indigo-900">
       <div class="bg-black bg-opacity-40 p-4 px-8 flex justify-between">
         <div>
@@ -14,12 +12,9 @@
           </h3>
         </div>
         <div>
-          <button
-            v-if="command.arguments.length > 0 || command.options.length > 0"
-            @click="collapsed = !collapsed"
+          <button v-if="command.arguments.length > 0 || command.options.length > 0" @click="collapsed = !collapsed"
             class="p-2 bg-transparent border-2 text-sm text-gray-200 border-indigo-500 rounded hover:bg-indigo-500 hover:text-gray-100 focus:border-indigo-300"
-            :class="{ 'bg-indigo-500': collapsed}"
-          >
+            :class="{ 'bg-indigo-500': collapsed }">
             GENERATE
           </button>
         </div>
@@ -27,10 +22,7 @@
     </div>
 
     <div class="px-8 py-4 space-y-2" v-if="hasParams(command)">
-      <div
-        v-if="command.options.length"
-        class="text-sm text-gray-700 dark:text-gray-300"
-      >
+      <div v-if="command.options.length" class="text-sm text-gray-700 dark:text-gray-300">
         <p class="font-bold text-lg">Options</p>
         <ul class="list-none list-inside space-y-1">
           <li v-for="option in command.options" :key="option.name">
@@ -42,10 +34,7 @@
         </ul>
       </div>
 
-      <div
-        v-if="command.arguments.length"
-        class="text-sm text-gray-700 dark:text-gray-300"
-      >
+      <div v-if="command.arguments.length" class="text-sm text-gray-700 dark:text-gray-300">
         <p class="font-bold text-lg">Arguments</p>
         <ul class="list-none list-inside space-y-1">
           <li v-for="argument in command.arguments" :key="argument.name">
@@ -60,55 +49,28 @@
     </div>
 
     <div
-      class="group relative overflow-hidden rounded-b-lg bg-indigo-700 bg-opacity-10 dark:bg-indigo-400 dark:bg-opacity-40"
-    >
+      class="group relative overflow-hidden rounded-b-lg bg-indigo-700 bg-opacity-10 dark:bg-indigo-400 dark:bg-opacity-40">
       <div class="flex items-center">
         <div class="flex-initial">
           <pre
-            class="scrollbar-none overflow-hidden overflow-x-auto p-6 px-8 text-sm leading-snug text-indigo-900 whitespace-pre-wrap dark:text-gray-300"
-          >
-php artisan {{ command.synopsis }}</pre
-          >
+            class="scrollbar-none overflow-hidden overflow-x-auto p-6 px-8 text-sm leading-snug text-indigo-900 whitespace-pre-wrap dark:text-gray-300">
+php artisan {{ command.synopsis }}</pre>
         </div>
         <div class="pr-8 flex-1 text-right">
-          <button
-            type="button"
+          <button type="button"
             class="group-hover:opacity-100 focus:opacity-100 opacity-0 text-indigo-900 transition duration-200"
             :class="{ 'focus:outline-none': !keyboardUsed }"
-            @click="copyCommand($event, `php artisan ${command.name}`)"
-          >
+            @click="copyCommand($event, `php artisan ${command.name}`)">
             <span class="sr-only">Click to copy</span>
-            <svg
-              v-if="copied"
-              class="w-6 h-6"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-              />
+            <svg v-if="copied" class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+              viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4" />
             </svg>
-            <svg
-              v-else
-              class="w-6 h-6"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
-              />
+            <svg v-else class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+              viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
             </svg>
           </button>
         </div>
