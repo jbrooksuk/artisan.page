f0f28cbfed8051397f56ff896541a46c93cc5911
James Brooks <james@alt-three.com>
2023-10-26 11:48:01 +0100

    wip

--- build.php
+++ build.php
@@ -1,37 +1,37 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+use Illuminate\Contracts\Console\Kernel;
 
-require_once __DIR__.'/bootstrap/app.php';
+require __DIR__.'/vendor/autoload.php';
 
-use Illuminate\Support\Facades\Artisan;
+$app = require __DIR__.'/bootstrap/app.php';
 
-$commands = Artisan::all();
+$commands = $app->make(Kernel::class)->all();
 
 echo collect($commands)->sortBy(function ($command) {
-    return $command->getName();
+  return $command->getName();
 })->map(function ($command) {
-    return [
-        'name' => $command->getName(),
-        'description' => $command->getDescription(),
-        'synopsis' => $command->getSynopsis(),
-        'definition' => $command->getDefinition(),
-        'aliases' => $command->getAliases(),
-        'arguments' => collect($command->getDefinition()->getArguments())->map(function ($argument) {
-            return [
-                'name' => $argument->getName(),
-                'description' => $argument->getDescription(),
-                'default' => $argument->getDefault(),
-                'required' => $argument->isRequired(),
-            ];
-        })->values()->all(),
-        'options' => collect($command->getDefinition()->getOptions())->map(function ($option) {
-            return [
-                'name' => $option->getName(),
-                'description' => $option->getDescription(),
-                'value_required' => $option->isValueRequired(),
-                'value_optional' => $option->isValueOptional(),
-            ];
-        })->values()->all(),
-    ];
+  return [
+    'name' => $command->getName(),
+    'description' => $command->getDescription(),
+    'synopsis' => $command->getSynopsis(),
+    'definition' => $command->getDefinition(),
+    'aliases' => $command->getAliases(),
+    'arguments' => collect($command->getDefinition()->getArguments())->map(function ($argument) {
+      return [
+        'name' => $argument->getName(),
+        'description' => $argument->getDescription(),
+        'default' => $argument->getDefault(),
+        'required' => $argument->isRequired(),
+      ];
+    })->values()->all(),
+    'options' => collect($command->getDefinition()->getOptions())->map(function ($option) {
+      return [
+        'name' => $option->getName(),
+        'description' => $option->getDescription(),
+        'value_required' => $option->isValueRequired(),
+        'value_optional' => $option->isValueOptional(),
+      ];
+    })->values()->all(),
+  ];
 })->values()->toJson();
