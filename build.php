e18d978e52f9f2f3f325e434d470bfeb30cfb3f1
James Brooks <james@alt-three.com>
2023-10-26 11:49:39 +0100

    Laravel 5.x support (#36)

---
+++ build.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Contracts\Console\Kernel;
+
+require __DIR__.'/vendor/autoload.php';
+
+$app = require __DIR__.'/bootstrap/app.php';
+
+$commands = $app->make(Kernel::class)->all();
+
+echo collect($commands)->sortBy(function ($command) {
+  return $command->getName();
+})->map(function ($command) {
+  return [
+    'name' => $command->getName(),
+    'description' => $command->getDescription(),
+    'synopsis' => $command->getSynopsis(),
+    'definition' => $command->getDefinition(),
+    'aliases' => $command->getAliases(),
+    'arguments' => collect($command->getDefinition()->getArguments())->map(function ($argument) {
+      return [
+        'name' => $argument->getName(),
+        'description' => $argument->getDescription(),
+        'default' => $argument->getDefault(),
+        'required' => $argument->isRequired(),
+      ];
+    })->values()->all(),
+    'options' => collect($command->getDefinition()->getOptions())->map(function ($option) {
+      return [
+        'name' => $option->getName(),
+        'description' => $option->getDescription(),
+        'value_required' => $option->isValueRequired(),
+        'value_optional' => $option->isValueOptional(),
+      ];
+    })->values()->all(),
+  ];
+})->values()->toJson();
