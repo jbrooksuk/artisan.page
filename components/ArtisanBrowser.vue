6d723f20463c23e741020ef5caeaafd362d23679
Amirul <naimteehee@gmail.com>
2023-10-27 17:26:40 +0800

    Add sticky position to Available Commands (#33)
    Co-authored-by: James Brooks <james@alt-three.com>

--- components/ArtisanBrowser.vue
+++ components/ArtisanBrowser.vue
@@ -8,7 +8,7 @@
 
     <main>
       <div class="flex my-4">
-        <div class="hidden md:block md:w-1/4 pr-4 space-y-4">
+        <div class="hidden sticky top-0 overflow-scroll h-screen md:block md:w-1/4 pr-4 space-y-4">
           <h2 class="text-xl font-bold text-gray-900 dark:text-gray-500">
             Commands
             <span class="text-xs text-gray-500 dark:text-gray-200">
