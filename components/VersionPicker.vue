5471270686b5d298eebce6c1d2be401ba2749825
James Brooks <james@alt-three.com>
2023-11-02 15:04:27 +0000

    Improve version picker UI

--- components/VersionPicker.vue
+++ components/VersionPicker.vue
@@ -1,12 +1,12 @@
 <script setup>
-import manifest from '../manifest.json'
+import { laravel } from '../manifest.json'
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue'
+import { ChevronDownIcon } from '@heroicons/vue/20/solid'
 
-const currentVersion = ref(useRoute().params.version || manifest['laravel'][0])
+const currentVersion = ref(useRoute().params.version || laravel[0])
 
-const changeVersion = () => {
-  const version = currentVersion.value
-
-  if (version && !manifest.laravel.includes(version)) {
+const changeVersion = (version) => {
+  if (version && !laravel.includes(version)) {
     useError({
       statusCode: 404,
       message: `Laravel version ${version} not found.`,
@@ -27,20 +27,24 @@ const changeVersion = () => {
 <template>
   <div class="flex-1 w-full">
     <label for="current-version" class="sr-only">Laravel Version</label>
-    <select
-      name="current-version"
-      id="current-version"
-      v-model="currentVersion"
-      @change="changeVersion()"
-      class="block w-full rounded-md border-0 py-1.5 pr-14 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6 dark:bg-gray-800 dark:text-gray-300 dark:ring-gray-700"
-    >
-      <option
-        v-for="version in manifest.laravel"
-        :value="version"
-        :key="version"
-      >
-        Laravel {{ version }}
-      </option>
-    </select>
+
+    <Menu as="div" class="relative inline-block text-left">
+      <div>
+        <MenuButton class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          {{ currentVersion }}
+          <ChevronDownIcon class="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+        </MenuButton>
+      </div>
+
+      <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+        <MenuItems class="absolute left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div class="py-1">
+            <MenuItem v-for="version in laravel" :key="version" v-slot="{ active }" as="div">
+              <NuxtLink :to="`/${version}/`" @click.prevent="changeVersion(version)" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']">{{ version }}</NuxtLink>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </transition>
+    </Menu>
   </div>
 </template>
