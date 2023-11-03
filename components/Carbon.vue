fe310eeda344309bba3a7a0b863b91607d0bc035
James Brooks <james@alt-three.com>
2023-11-03 14:29:47 +0000

    Improve ad layout

--- components/Carbon.vue
+++ components/Carbon.vue
@@ -18,13 +18,13 @@ onMounted(() => {
 <template>
   <div
     ref="carbonads"
-    class="Carbon border border-gray-200 dark:border-gray-800 rounded-lg bg-white dark:bg-gray-900"
+    class="Carbon border border-gray-200 dark:border-gray-800 rounded-lg bg-white dark:bg-gray-950"
   ></div>
 </template>
 
 <style>
 .dark .Carbon .carbon-text {
-  @apply text-gray-400 hover:text-gray-200;
+  @apply text-gray-400 hover:text-gray-300;
 }
 
 .light .Carbon .carbon-text {
@@ -32,53 +32,30 @@ onMounted(() => {
 }
 
 .Carbon {
-  @apply p-3;
   @apply p-3 flex flex-col max-w-full md:fixed md:right-4 md:bottom-4 md:w-40;
 
   #carbonads span {
     @apply flex flex-col justify-between;
 
     .carbon-wrap {
-      @apply flex flex-row sm:flex-col flex-1;
-
-      @apply lg:flex-col;
+      @apply flex flex-row flex-1 sm:flex-col;
 
       .carbon-img {
-        @apply flex items-start justify-center mb-4;
-
-        /*@media (min-width: 320px) {
-          @apply mb-0;
-        }*/
-
-        @apply lg:mb-4;
+        @apply flex items-start justify-center mr-4 mb-0 sm:mb-4 lg:mr-0;
       }
 
       .carbon-text {
-        @apply flex-1 text-sm w-full m-0 text-left block;
-
-        &:hover {
-          @apply no-underline;
-        }
-
-        /*@media (min-width: 320px) {
-          @apply ml-4;
-        }*/
-
-        @apply lg:ml-0;
+        @apply flex-1 text-sm w-full m-0 text-left block lg:ml-0;
       }
     }
   }
 
   img {
-    @apply w-full;
+    @apply w-full rounded-sm;
   }
 
   & .carbon-poweredby {
-    @apply ml-2 text-xs text-right text-gray-400 block pt-2;
-
-    &:hover {
-      @apply no-underline text-gray-500;
-    }
+    @apply ml-2 text-xs text-right text-gray-400 block pt-0 sm:pt-2 hover:no-underline hover:text-gray-500;
   }
 }
 </style>
