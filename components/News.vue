738d798b403ae6efe6652e2cce2e46117185aab0
James Brooks <james@alt-three.com>
2024-02-21 20:14:10 +0000

    Update News

--- components/News.vue
+++ components/News.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Banner in Tailwind -->
-  <div class="bg-amber-200 text-amber-900 dark:bg-slate-800 dark:text-slate-400 py-3 px-4 text-sm md:py-2 md:px-3 md:text-xs border-b border-amber-600 dark:border-0 text-center">
-    The Laravel Artisan Cheatsheet now has an API! <a href="https://james.brooks.page/blog/laravel-artisan-cheatsheet-api" class="underline underline-offset-2 font-semibold">Learn more</a>.
+  <div class="bg-amber-100 text-amber-700 dark:bg-slate-800 dark:text-slate-400 p-3 text-sm md:text-base md:py-4 md:p-3 border-b border-amber-400 dark:border-0 text-center">
+    The Laravel Artisan Cheatsheet now has an API. <a href="https://james.brooks.page/blog/laravel-artisan-cheatsheet-api" class="underline underline-offset-2 font-semibold">Learn more</a>.
   </div>
 </template>
