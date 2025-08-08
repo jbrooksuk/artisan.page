b5fe73a879e375298ccc7f7ddc1b0ad4246dcfc9
James Brooks <james@alt-three.com>
2025-08-08 08:58:23 +0100

    Support 12.x in the API

--- server/api/[version].js
+++ server/api/[version].js
@@ -1,6 +1,7 @@
 import { laravel } from '../../manifest.json'
 
 const versionMap = {
+  '12.x': () => import(`~/assets/12.x.json`),
   '11.x': () => import(`~/assets/11.x.json`),
   '10.x': () => import(`~/assets/10.x.json`),
   '9.x': () => import(`~/assets/9.x.json`),
